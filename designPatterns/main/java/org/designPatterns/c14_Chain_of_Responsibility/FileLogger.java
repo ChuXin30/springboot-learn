3d63d0d7efcd93a09de86c27a60ce9d77d1b4032
haochen <957652322@qq.com>
2024-07-14 22:38:17 +0800

    设计模式：责任链模式14

---
+++ designPatterns/main/java/org/designPatterns/c14_Chain_of_Responsibility/FileLogger.java
@@ -0,0 +1,13 @@
+package org.designPatterns.c14_Chain_of_Responsibility;
+
+public class FileLogger extends AbstractLogger {
+
+    public FileLogger(int level){
+        this.level = level;
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("File::Logger: " + message);
+    }
+}
